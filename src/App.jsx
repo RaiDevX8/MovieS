d5de7e6f22ee0b17d2417afd93941cc1bfaa2a0e
RaiDevX8 <charanrai193@gmail.com>
2024-06-19 19:10:01 +0530

    add:header done

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,51 @@ import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getApiconfig } from './store/slice/HomeSlice'
 import { fetchDataFromApi } from './utils/api'
+//imports
 
+import Footer from './components/footer/Footer'
+import Header from './components/header/Header'
+import PageNotfound from './pages/404/PageNotfound'
+import Detail from './pages/Detail/Detail'
+import Explore from './pages/Explore/Explore'
+import Home from './pages/Home/Home'
+import SearchResult from './pages/SearchResult/SearchResult'
+
+
+import { BrowserRouter,Route,Routes } from 'react-router-dom'
 const App = () => {
   const dispatch = useDispatch()
   const { url } = useSelector(state => state.home)
-  const [data, setData] = useState([]) // Correct placement of useState
+
 
   useEffect(() => {
-    const apitesting = () => {
-      fetchDataFromApi('/movie/popular').then(res => {
-        console.log(res)
-        dispatch(getApiconfig(res))
-        setData(res.results)
+    const FetchApiConfig = () => {
+      fetchDataFromApi('/configuration').then(res => {
+        console.log(res);
+        const url={
+          backdrop:res.images.secure_base_url + "original",
+          poster:res.images.secure_base_url + "original",
+          profile:res.images.secure_base_url + "original",
+
+        }
+        dispatch(getApiconfig(url))
       })
     }
 
-    apitesting()
+    FetchApiConfig()
   }, [dispatch])
-  const imageUrl = 'https://image.tmdb.org/t/p/w500'
   return (
-    <>
-      {data.map((item, index) => (
-        <div key={index}>
-          <p key={index}>{item.title}</p>
-          <img src={`${imageUrl}${item.poster_path}`} alt="" />
-        </div>
-      ))}
-    </>
+    <BrowserRouter>
+    <Header/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:mediaType/:id" element={<Detail />} />
+        <Route path="search/:query" element={<SearchResult />} />
+        <Route path="/explore/:mediaType" element={<Explore />} />
+        <Route path='*' element={<PageNotfound/>} />
+      </Routes>
+      <Footer/>
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/contentWrapper/ContentWrapper.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './Style.scss'
+const ContentWrapper = ({children}) => {
+  return <div className="contentwrapper">{children}</div>
+
+}
+
+export default ContentWrapper
